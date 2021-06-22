ac3497c49f419fed9a55c8970cca4083c0562c00
Kobi Hari <kobi2294@yahoo.com>
2021-06-22 14:01:29 +0300

    exercise 3 solution

--- Day 03/ex3-solution/src/app/app.component.ts
+++ Day 03/ex3-solution/src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //Data
+  a: number = 0;
+  b: number = 0;
+  sum: number = 0;
+  product: number = 0;
+  difference: number = 0;
+
+  calc(a: number, b: number) {
+    this.a = a;
+    this.b = b;
+    this.sum = a + b;
+    this.difference = a - b;
+    this.product = a * b;
+  } 
 }
+
+
