631c187cd7855d411d7cbc2cfb76c2f9571c9022
Kobi Hari <kobi2294@yahoo.com>
2021-06-28 14:45:09 +0300

    examples of operators

--- Day 05/rxjs-colors/src/app/app.component.ts
+++ Day 05/rxjs-colors/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { from, interval, Observable, of } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxjs-colors';
+  mashu$!: Observable<any>;
+
+  constructor() {
+    this.mashu$ = interval(1000).pipe(
+      map(i => i ** 2), 
+      filter(i => i % 3 === 0),
+      tap(val => console.log(val))
+    );
+  }
+
+
+  // factories: 
+  // 1. interval
+  // 2. of
+  // 3. from
 }
