1a5579282d0a7583c1b057ce690e960a8af2570c
Kobi Hari <kobi2294@yahoo.com>
2021-07-01 14:18:22 +0300

    ex 5 solution - step 1

---
+++ Day 06/ex5-solution/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { AddMovieComponent } from './components/add-movie/add-movie.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'movies', pathMatch: 'full'},
+  { path: 'movies', component: MoviesListComponent }, 
+  { path: 'add', component: AddMovieComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MoviesListComponent,
+    AddMovieComponent
+  ],
+  imports: [
+    BrowserModule, 
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
