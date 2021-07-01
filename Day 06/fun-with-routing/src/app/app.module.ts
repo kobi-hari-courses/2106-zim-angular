03e7bbca230b47212b1734a1f5d0414bb5fe62c6
Kobi Hari <kobi2294@yahoo.com>
2021-07-01 13:54:41 +0300

    fun with routing

--- Day 06/fun-with-routing/src/app/app.module.ts
+++ Day 06/fun-with-routing/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { PageAComponent } from './components/page-a/page-a.component';
+import { PageBComponent } from './components/page-b/page-b.component';
+import { PageCComponent } from './components/page-c/page-c.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'a', pathMatch: 'full'},
+  {path: 'a', component: PageAComponent}, 
+  {path: 'b', component: PageBComponent}, 
+  {path: 'c', component: PageCComponent}, 
+  {path: '**', component: NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageAComponent,
+    PageBComponent,
+    PageCComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
