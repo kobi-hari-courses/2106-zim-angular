547f62e3b7974761d85d822b02322af621e10e9e
Kobi Hari <kobi2294@yahoo.com>
2021-07-01 14:33:50 +0300

    ex5 step 3

--- Day 06/ex5-solution/src/app/components/movies-list/movies-list.component.ts
+++ Day 06/ex5-solution/src/app/components/movies-list/movies-list.component.ts
@@ -1,4 +1,7 @@
+import { DataService } from './../../service/data.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Movie } from 'src/app/models/movie.model';
 
 @Component({
   selector: 'app-movies-list',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-list.component.css']
 })
 export class MoviesListComponent implements OnInit {
+  movies$!: Promise<Movie[]>;
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.movies$ = this.data.getAllMovies();
   }
 
 }
